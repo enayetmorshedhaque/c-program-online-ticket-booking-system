5f6264f41509868171c0f6f241b5ca33aeaae1a1
Enayet Morshed Haque <32461477+enayetmorshedhaque@users.noreply.github.com>
2018-07-16 01:30:52 +0600

    Update ticket_booking_management_system.c

--- ticket_booking_management_system.c
+++ ticket_booking_management_system.c
@@ -2,8 +2,9 @@
 #include <string.h>
 #include <time.h>
 
-int option; //global variables
-char user_name[20], password[20]; //global variables
+
+int option;
+char user_name[20], password[20], source[20], destination[20];
 void getCurrentTime();
 void login();
 void createAccount();
@@ -12,23 +13,31 @@ void trainRoute();
 void purchaseTicket();
 void logout();
 
+
+struct compartments{
+
+    int total_seats, booked_seats, available_seats;
+    float ticket_fair;
+
+}f_chair, f_seat, shovan, s_chair;
+
 int main(){
     char user_name[20], password[15];
 
     printf("\t\t\t\t------------------------------------------------------------\n");
-    printf("\t\t\t\t\t   WELCOME TO ONLINE TICKET BOOKING SYSTEM\n");
+    printf("\t\t\t\t\t   BANGLADESH RAILWAY E-TICKETING SERVICE\n");
     printf("\t\t\t\t------------------------------------------------------------\n\n\n\n\n");
 
-    getCurrentTime(); //gets the current time
+    getCurrentTime();
     printf("1. SignIn\n");
     printf("2. Create Account\n\n");
     printf("Enter Your Choice: ");
     scanf("%d",&option);
     switch(option){
-        case 1: login(); //login function is called here
+        case 1: login();
                 break;
 
-        case 2: createAccount(); //create account function is called here
+        case 2: createAccount();
                 break;
 
         default: printf("Wrong Choice!!!\n");
@@ -40,14 +49,13 @@ int main(){
 
 
 void getCurrentTime(){
-    
+
     time_t rawtime;
     struct tm * timeinfo;
 
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     printf("Current Time and Date: %s\n", asctime(timeinfo));
-
 }
 
 void login(){
@@ -59,7 +67,7 @@ void login(){
     scanf("%s",&user_name);
     printf("enter your password: ");
     scanf("%s",&password);
-    if(strcmp(user_name, "admin")== 0 && strcmp(password, "admin123")==0){ //checking the user name and password
+    if(strcmp(user_name, "admin@admin.admin")== 0 && strcmp(password, "admin123")==0){
 
         printf("\n\t\t-----------------------------------------------\n");
         printf("\t\t\t   WELCOME TO YOUR DASHBOARD USER\n");
@@ -93,13 +101,30 @@ void login(){
 
 void fareQuery(){
 
+    printf("\n\t\t-----------------------------------------------\n");
+    printf("\t\t\t   FARE QUERY OF TRAINS\n");
+    printf("\t\t-----------------------------------------------\n\n");
+
 }
 
 void trainRoute(){
 
+    printf("\n\t\t-----------------------------------------------\n");
+    printf("\t\t\t   ROUTE DETAILS OF AVAILABLE TRAINS\n");
+    printf("\t\t-----------------------------------------------\n\n");
+
 }
 
 void purchaseTicket(){
+
+    printf("\n\t\t-----------------------------------------------\n");
+    printf("\t\t\t   PURCHASE YOUR TICKET HERE\n");
+    printf("\t\t-----------------------------------------------\n\n");
+    printf("Station From: ");
+    scanf("%s",&source);
+    printf("Station To: ");
+    scanf("%s",&destination);
+    //printf("")
 
 }
 
